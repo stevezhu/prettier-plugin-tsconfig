af7adfb7bcc22dc67a1444fb72a189922fd6b30f
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2025-10-28 19:13:25 -0400

    Change eslint config to ts

--- eslint.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig, globalIgnores } from '@stzhu/eslint-config';
-import tsConfig from '@stzhu/eslint-config/ts';
-import vitestConfig from '@stzhu/eslint-config/vitest';
-
-export default defineConfig(globalIgnores(['dist']), tsConfig, vitestConfig);
